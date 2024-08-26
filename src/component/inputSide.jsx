9eaaeeae02fd09f61f8f1d6043a30dbfa6440be0
symonmuchemi <simonkenya846@gmail.com>
2024-08-26 04:36:50 +0300

    update: added individual id to buttons and rearranged labels and inputs

--- src/component/inputSide.jsx
+++ src/component/inputSide.jsx
@@ -2,19 +2,23 @@ import "./inputSide.css";
 
 function InputSide() {
   return (
-    <div>
+    <div className="container">
       <h2>Paste code here:</h2>
       <textarea />
       <h2>Check for:</h2>
       <div className="options">
-        <input type="checkbox" name="code-smell" id="code-smell" />
-        <label htmlFor="code-smell">Code smell</label>
-        <input type="checkbox" name="optimization" id="optimization" />
-        <label htmlFor="Optimization">Optimization</label>
+        <label htmlFor="code-smell">
+          <input type="checkbox" name="code-smell" id="code-smell" />
+          Code smell
+        </label>
+        <label htmlFor="Optimization">
+          <input type="checkbox" name="optimization" id="optimization" />
+          Optimization
+        </label>
       </div>
       <div className="buttons">
-        <button>Get Code Review</button>
-        <button>Get Code from GitHub</button>
+        <button id="review">Get Code Review</button>
+        <button id="github">Get Code from GitHub</button>
       </div>
     </div>
   );

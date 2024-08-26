2f18911928fe542c1fcbe21ceedc1800eb461275
symonmuchemi <simonkenya846@gmail.com>
2024-08-26 04:59:54 +0300

    feat: open link in new tab

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,10 @@ function Footer() {
   return (
     <footer>
       <p>
-        Made with 🧡 by <a href="https://github.com/symonmuchemi">Simon Muchemi</a>
+        Made with 🧡 by{" "}
+        <a href="https://github.com/symonmuchemi" target="_blank">
+          Simon Muchemi
+        </a>
       </p>
     </footer>
   );
